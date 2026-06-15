427569a113631947080bc5af3a8e292a59648e01
Natalie Lynn Rittenhouse (nathouse) <nrittenhouse3@gmail.com>
2026-06-15 13:28:13 -0400

    update run.sh

--- src/run.sh
+++ src/run.sh
@@ -1,12 +1,13 @@
-
+#!/bin/bash
 
 cur_dir=$(pwd)
-nlayer = 3
 ndim = 25
 
 ## train the model
 for lr in 0.01 0.001 0.0001; do
-    python ${cur_dir}/cavebear_pytorch_cvae.py --learning_rate ${lr} --nlayer ${nlayer} --embed_dim ${ndim} --predict train --train_species mouse --target_species zebrafish
+    for nlayer in 2 3 4; do
+        python ${cur_dir}/cavebear_pytorch_cvae.py --learning_rate ${lr} --nlayer ${nlayer} --embed_dim ${ndim} --predict train --train_species mouse --target_species zebrafish
+    done
 done
 
 
